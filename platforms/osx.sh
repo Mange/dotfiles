1886ae306f66a4830438e3044015b9b6578ab683
Magnus Bergmark <magnus.bergmark@gmail.com>
2014-03-21 14:23:42 +0100

    Fixes to the OSX bootstrapping scripts

--- platforms/osx.sh
+++ platforms/osx.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
+cd $(dirname $0)
+
 # Keyboard layout
-keyboardfile="$HOME/Library/Keyboard Layouts/Manges.keylayout"
 if [ ! -e "$keyboardfile" ]; then
   echo "::::::::::: INSTALLING KEYBOARD LAYOUT :::::::::::"
+  keyboardfile="$HOME/Library/Keyboard Layouts/Manges.keylayout"
   rm -f "$keyboardfile" # In case we had a broken symlink rather than nothing at all
-  ln -s "$(realpath $(dirname $0))/osx/Manges.keylayout" "$keyboardfile"
+  ln -s "$(pwd -P)/osx/Manges.keylayout" "$keyboardfile"
 fi
 
 if [ ! -e /usr/local/bin/brew ]; then
@@ -14,7 +16,22 @@ if [ ! -e /usr/local/bin/brew ]; then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
+echo "::::::::::: INSTALLING SOFTWARE :::::::::::"
 brew bundle osx/Brewfile
+
+if ! ps aux | grep "Alfred 2\.app" > /dev/null 2>&1; then
+  if [ -d "$HOME/Applications/Alfred 2.app" ]; then
+    echo "::::::::::: STARTING ALFRED :::::::::::"
+    open "$HOME/Applications/Alfred 2.app"
+    sleep 5
+    brew cask alfred link
+  fi
+fi
+
+if [ ! -d $HOME/.rvm ]; then
+  echo "::::::::::: INSTALLING RVM :::::::::::"
+  \curl -sSL https://get.rvm.io | bash -s stable
+fi
 
 # zsh-completions
 chmod go-w /usr/local/share
