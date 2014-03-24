095ef5129dc7a0fc8c6d41bb6dedb45840f9658d
Magnus Bergmark <magnus.bergmark@gmail.com>
2014-03-24 16:28:32 +0100

    Put keyboard layout in system directory to allow it in all apps

--- platforms/osx.sh
+++ platforms/osx.sh
@@ -7,8 +7,7 @@ cd $(dirname $0)
 keyboardfile="/Library/Keyboard Layouts/Manges.keylayout"
 if [ ! -e "$keyboardfile" ]; then
   echo "::::::::::: INSTALLING KEYBOARD LAYOUT :::::::::::"
-  rm -f "$keyboardfile" # In case we had a broken symlink rather than nothing at all
-  ln -s "$(pwd -P)/osx/Manges.keylayout" "$keyboardfile"
+  sudo cp "$(pwd -P)/osx/Manges.keylayout" "$keyboardfile"
 fi
 
 if [ ! -e /usr/local/bin/brew ]; then
