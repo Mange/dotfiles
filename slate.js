9ced9dd783732ae2caff917bbeb4304b17318494
Magnus Bergmark <magnus.bergmark@gmail.com>
2014-09-26 09:30:43 +0200

    Implement automatic layouts for Slate

--- slate.js
+++ slate.js
@@ -17,6 +17,54 @@ S.cfga({
 var monitorPrimary = "0";
 var monitorSecondary = "1";
 
+// Actions
+var left = S.op("push", {direction: "left", style: "bar-resize:screenSizeX/2"});
+var leftPrimary = left.dup({screen: monitorPrimary});
+
+var right = S.op("push", {direction: "right", style: "bar-resize:screenSizeX/2"});
+var rightPrimary = right.dup({screen: monitorPrimary});
+
+var up = S.op("push", {direction: "up", style: "bar-resize:screenSizeY/2"});
+var down = S.op("push", {direction: "down", style: "bar-resize:screenSizeY/2"});
+
+var fullscreen = S.op("corner", {direction: "top-left", height: "screenSizeY", width: "screenSizeX"});
+var fullscreenSecondary = fullscreen.dup({screen: monitorSecondary});
+
+var hide = S.op("hide");
+var show = S.op("show");
+
+var fullscreenFlowdock = fullscreen.dup({app: "Flowdock"});
+var hideFlowdock = hide.dup({app: "Flowdock"});
+var showFlowdock = show.dup({app: "Flowdock"});
+
+var bothScreens = function(op) {
+  var ops = [];
+  S.eachScreen(function(screen) {
+    ops.push(op.dup({screen: screen}));
+  });
+  return S.op("chain", {operations: ops});
+};
+
+// Layouts //
+
+// Single screen layout
+var singleScreenLayout = S.layout("single screen", {
+  "_before_": {operations: [fullscreenFlowdock, hideFlowdock]},
+  "MacVim": {operations: [left, right], repeat: true},
+  "iTerm": {operations: [right], repeat: true}
+});
+
+// Two screen layout
+var twoScreenLayout = S.layout("two screens", {
+  "_before_": {operations: [showFlowdock]},
+  "MacVim": {operations: [leftPrimary, rightPrimary], repeat: true},
+  "iTerm": {operations: [rightPrimary], repeat: true},
+  "Flowdock": {operations: [fullscreenSecondary]}
+});
+
+S.default(1, singleScreenLayout);
+S.default(2, twoScreenLayout);
+
 // Binds //
 
 var hyperKey = function(keyName) {
@@ -39,26 +87,25 @@ S.bind(hyperKey("u"), S.op("focus", {app: "Dash"}), false);
 
 // Move + Resize
 
-// Push window in a direction and style, cycling between the monitors
-var pushWindow = function(direction, style) {
-  return S.op("chain", {operations: [
-    S.op("push", {direction: direction, style: style, screen: monitorPrimary}),
-    S.op("push", {direction: direction, style: style, screen: monitorSecondary})
-  ]});
-};
+S.bind(hyperKey("left"), bothScreens(left));
+S.bind(hyperKey("right"), bothScreens(right));
+S.bind(hyperKey("up"), bothScreens(up));
+S.bind(hyperKey("down"), bothScreens(down));
 
-S.bind(hyperKey("left"), pushWindow("left", "bar-resize:screenSizeX/2"));
-S.bind(hyperKey("right"), pushWindow("right", "bar-resize:screenSizeX/2"));
-
-S.bind(hyperKey("up"), pushWindow("up", "bar-resize:screenSizeY/2"));
-S.bind(hyperKey("down"), pushWindow("down", "bar-resize:screenSizeY/2"));
-
-S.bind(hyperKey("return"), S.op("chain", {operations: [
-  S.op("corner", {direction: "top-left", height: "screenSizeY", width: "screenSizeX", screen: monitorPrimary}),
-  S.op("corner", {direction: "top-left", height: "screenSizeY", width: "screenSizeX", screen: monitorSecondary})
-]}));
+S.bind(hyperKey("return"), bothScreens(fullscreen));
 
 // Misc
 S.bind(hyperKey("r"), S.op("relaunch"));
 
-S.bind(hyperKey("space"), S.op("hint", {characters: "JKLN1234567890"}));
+S.bind(hyperKey("f"), S.op("hint", {characters: "JKLN1234567890"}));
+
+// Reapply layout
+S.bind(hyperKey("space"), function() {
+  var layoutName;
+  if (S.screenCount() == 1) {
+    layoutName = singleScreenLayout;
+  } else {
+    layoutName = twoScreenLayout;
+  }
+  S.op("layout", {name: layoutName}).run();
+});
