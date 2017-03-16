f5142fb25c939adb77f46f61dde4b171d6e6f8a0
Magnus Bergmark <magnus.bergmark@gmail.com>
2017-03-16 10:42:49 +0100

    Replace Linux Spotify client with Mopidy

--- platforms/ubuntu.sh
+++ platforms/ubuntu.sh
@@ -47,6 +47,15 @@ install-chrome() {
   rm google-chrome*.deb
 }
 
+install-mopidy-apt-source() {
+  if [[ ! -e /etc/apt/sources.list.d/mopidy.list ]]; then
+    header "Adding mopidy APT source"
+
+    wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+    sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
+  fi
+}
+
 install-i3-gaps() {
   # From: https://github.com/Airblader/i3/wiki/Compiling-&-Installing#ubuntu--1610
   apt-install \
@@ -83,6 +92,8 @@ handle-failure() {
 
 # Ask for password right away.
 sudo echo > /dev/null
+
+install-mopidy-apt-source || handle-failure
 
 header "Refreshing apt cache"
 sudo apt -qq update
