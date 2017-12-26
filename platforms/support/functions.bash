41bc290e8f708d9dbc090cc5013c43056eae9a65
Magnus Bergmark <magnus.bergmark@gmail.com>
2017-12-26 19:46:57 +0100

    Fix some Bash idioms in support functions

--- platforms/support/functions.bash
+++ platforms/support/functions.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export reset=`tput sgr0`
-export black=`tput setaf 0`
-export red=`tput setaf 1`
-export green=`tput setaf 2`
-export yellow=`tput setaf 3`
-export blue=`tput setaf 4`
-export magenta=`tput setaf 5`
-export cyan=`tput setaf 6`
-export white=`tput setaf 7`
+export reset=$(set +e; tput sgr0)
+export black=$(set +e; tput setaf 0)
+export red=$(set +e; tput setaf 1)
+export green=$(set +e; tput setaf 2)
+export yellow=$(set +e; tput setaf 3)
+export blue=$(set +e; tput setaf 4)
+export magenta=$(set +e; tput setaf 5)
+export cyan=$(set +e; tput setaf 6)
+export white=$(set +e; tput setaf 7)
 
 overline() {
   printf "%0.s‾" `eval echo {1..$1}`
@@ -17,16 +17,16 @@ export -f overline
 
 header() {
   echo
-  if [[ $BASH_VERSION < 4 ]]; then
+  if [[ "$BASH_VERSION" < "4" ]]; then
     # Mac OS comes with a very old version of bash
     # No uppercase for me.
     local message="$1"
   else
     local message="${1^^}"
   fi
-  echo "$yellow‣ $green${message}$reset"
-  echo -n $yellow
+  echo "${yellow}‣ ${green}${message}${reset}"
+  echo -n "$yellow"
   overline $((${#1} + 2))
-  echo $reset
+  echo "$reset"
 }
 export -f header
