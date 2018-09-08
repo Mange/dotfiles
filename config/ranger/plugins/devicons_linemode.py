5112904d7596c69f17437f00e2f49e032df65a05
Magnus Bergmark <magnus.bergmark@gmail.com>
2018-09-09 00:34:15 +0200

    Ranger: Disable devicons in kitty due to pango bug
    I hope to get this fixed some time. Either Nerd Fonts add a space glyph,
    or pango stops assuming all fonts have a space.
    
    I think Nerd Fonts would be easier to fix with the whole Github culture
    and the fact that the problem comes when using their symbols, but you
    know... Maybe not.

--- config/ranger/plugins/devicons_linemode.py
+++ config/ranger/plugins/devicons_linemode.py
@@ -2,7 +2,7 @@ import ranger.api
 from ranger.core.linemode import LinemodeBase
 from devicons import *
 
-@ranger.api.register_linemode
+# @ranger.api.register_linemode
 class DevIconsLinemode(LinemodeBase):
   name = "devicons"
 
@@ -11,7 +11,7 @@ class DevIconsLinemode(LinemodeBase):
   def filetitle(self, file, metadata):
     return devicon(file) + ' ' + file.relative_path
 
-@ranger.api.register_linemode
+# @ranger.api.register_linemode
 class DevIconsLinemodeFile(LinemodeBase):
   name = "filename"
 
