6e8767dd421d07549e4f3c52ce627b6ccb2cd07d
Magnus Bergmark <magnus.bergmark@gmail.com>
2020-05-14 13:41:29 +0200

    Awesome: Disable some layouts I don't want
    Floating is no longer the default for all tags; it's not even enabled
    anymore.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -53,19 +53,19 @@ modkey = "Mod4" -- Super
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
+    -- awful.layout.suit.floating,     -- All windows are floating
+    awful.layout.suit.tile,            -- Master + Stack, with Master on Left
+    awful.layout.suit.tile.left,       -- Master + Stack, with Master on Right
+    -- awful.layout.suit.tile.bottom,  -- Master + Stack, with Master on Bottom
+    awful.layout.suit.tile.top,        -- Master + Stack, with Master on Top
+    awful.layout.suit.fair,            -- Grid (Vertical)
+    awful.layout.suit.fair.horizontal, -- Grid (Horizontal)
+    -- awful.layout.suit.spiral,       -- Fibonacci
+    awful.layout.suit.spiral.dwindle,  -- BSP
+    awful.layout.suit.max,             -- "Tabbed"
+    -- awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier,       -- Master floating center
+    -- awful.layout.suit.corner.nw,    -- Master + two stacks below and to the side
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
