8c4a4bdd28f592165bf980be2f93adfa5c20da62
Magnus Bergmark <magnus.bergmark@gmail.com>
2020-07-20 19:07:52 +0200

    Auto hibernate after some time, if suspended

--- config/awesome/keys.lua
+++ config/awesome/keys.lua
@@ -43,7 +43,7 @@ keys.awesome_chord = which_keys.new_chord(
       }),
 
       which_keys.key_nested("p", "power", {
-          which_keys.key("s", "suspend", actions.spawn({"sh", "-c", "udiskie-umount --all && systemctl suspend"})),
+          which_keys.key("s", "suspend", actions.spawn({"sh", "-c", "udiskie-umount --all && systemctl suspend-then-hibernate"})),
           which_keys.key("h", "hibernate", actions.spawn({"sh", "-c", "udiskie-umount --all && systemctl hibernate"})),
           which_keys.key("R", "reboot", actions.log_out("reboot", "--force")),
           which_keys.key("Q", "power-off", actions.log_out("poweroff", "--force")),
