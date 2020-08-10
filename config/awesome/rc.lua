7403fcc63addbb63958423ae9529d53f5c1c2224
Magnus Bergmark <me@mange.dev>
2020-08-10 22:51:18 +0200

    Awesome: Fold rc.lua file

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,3 +1,4 @@
+-- vim: foldmethod=marker
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
