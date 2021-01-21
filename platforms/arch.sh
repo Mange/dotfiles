61cf2dffc8c9b3bc732661090e676d659b5a1916
Magnus Bergmark <me@mange.dev>
2021-01-21 10:17:19 +0100

    Arch: Install and enable ssh server

--- platforms/arch.sh
+++ platforms/arch.sh
@@ -584,6 +584,7 @@ if run-section "fast"; then
   enable-systemd-unit "NetworkManager"
   enable-systemd-unit "lightdm"
   enable-systemd-unit "bluetooth"
+  enable-systemd-unit "sshd"
   enable-systemd-unit "pcscd" # smartcard daemon, for Yubikey, etc.
 
   if hash docker 2>/dev/null; then
