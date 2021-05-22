2ea76ac15c0349591ae47a64c01267524956506f
Magnus Bergmark <me@mange.dev>
2021-05-22 23:57:39 +0200

    Awesome: Hide top panel when going fullscreen

--- config/awesome/layout/init.lua
+++ config/awesome/layout/init.lua
@@ -6,3 +6,36 @@ screen.connect_signal(
     s.top_panel = top_panel(s)
   end
 )
+
+--
+-- Hide top panel when apps go fullscreen
+--
+function update_panel_visibility()
+  for s in screen do
+    if s.selected_tag then
+      local is_fullscreen = s.selected_tag.fullscreen_mode
+
+      s.top_panel.visible = not is_fullscreen
+    end
+  end
+end
+
+tag.connect_signal("property::selected", function(_) update_panel_visibility() end)
+client.connect_signal(
+  "property::fullscreen",
+  function(c)
+    if c.first_tag then
+      c.first_tag.fullscreen_mode = c.fullscreen
+    end
+    update_panel_visibility()
+  end
+)
+client.connect_signal(
+  "unmanage",
+  function(c)
+    if c.fullscreen then
+      c.screen.selected_tag.fullscreen_mode = false
+      update_panel_visibility()
+    end
+  end
+)
