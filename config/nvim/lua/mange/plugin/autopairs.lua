8495e1713c0a28ae2c754af5dbd9963e8277c80b
Magnus Bergmark <me@mange.dev>
2021-12-16 16:32:33 +0100

    Neovim: Fix annoying autopairs problem
    I had misunderstood what the code enabled due to a confusing README. The
    code snipped added "()" automatically after completing a function, which
    is extremely annoying.

--- config/nvim/lua/mange/plugin/autopairs.lua
+++ config/nvim/lua/mange/plugin/autopairs.lua
@@ -1,5 +1,4 @@
 local npairs = require("nvim-autopairs")
-local cmp = require("cmp")
 
 npairs.setup({
   disable_filetype = { "TelescopePrompt" },
@@ -9,15 +8,3 @@ npairs.setup({
 npairs.add_rules(require("nvim-autopairs.rules.endwise-elixir"))
 npairs.add_rules(require("nvim-autopairs.rules.endwise-lua"))
 npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
-
--- Setup smart <CR> handling; i.e. that: (| = Cursor)
---
---      {|}
---  -> Press <CR>
---  ->
---      {
---        |
---      }
---
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
