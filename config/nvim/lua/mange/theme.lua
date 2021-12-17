93ef24c27d67cf1cd60931f66d5a7fdd7868e27d
Magnus Bergmark <me@mange.dev>
2021-12-17 10:57:58 +0100

    Neovim: Change how diagnostics are rendered

--- config/nvim/lua/mange/theme.lua
+++ config/nvim/lua/mange/theme.lua
@@ -30,6 +30,34 @@ local function theme_shared()
     ]],
     false
   )
+
+  -- Highlight line number instead of showing a sign on lines with diagnostics
+  vim.cmd [[
+    highlight DiagnosticLineNrError guibg=#51202A guifg=#FF0000 gui=bold
+    highlight DiagnosticLineNrWarn guibg=#51412A guifg=#FFA500 gui=bold
+    highlight DiagnosticLineNrInfo guibg=#1E535D guifg=#00FFFF gui=bold
+    highlight DiagnosticLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
+
+    sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
+    sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
+    sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
+    sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
+  ]]
+
+  vim.diagnostic.config {
+    -- virtual_text = {
+    --   prefix = '●', -- Could be '■', '▎', 'x'
+    --   source = "always",
+    -- },
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    float = {
+      source = "always",
+    },
+    severity_sort = true,
+    update_in_insert = false,
+  }
 end
 
 local function theme_light()
