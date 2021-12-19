05665257cc19f28a103ae6f91b4b8d878c168124
Magnus Bergmark <me@mange.dev>
2021-12-20 00:58:50 +0100

    Wezterm: Switch to Catppuccino

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,69 @@
 local wezterm = require "wezterm"
 
 return {
-  color_scheme = "Gruvbox Dark",
+  color_scheme = "Catppuccino",
+  color_schemes = {
+    -- https://github.com/catppuccin/catppuccin/issues/17
+    Catppuccino = {
+      background = "#1E1E29",
+      foreground = "#D7DAE0",
+      selection_bg = "#2D293B",
+      selection_fg = "#F0AFE1",
+      cursor_bg = "#B3E1A3",
+      cursor_fg = "#EADDA0",
+      cursor_border = "#B3E1A3",
+
+      scrollbar_thumb = "#B3E1A3",
+
+      split = "#B3E1A3",
+
+      ansi = {
+        "#6E6C7C",
+        "#E28C8C",
+        "#B3E1A3",
+        "#EADDA0",
+        "#A4B9EF",
+        "#C6AAE8",
+        "#F0AFE1",
+        "#D7DAE0",
+      },
+      brights = {
+        "#6E6C7C",
+        "#E28C8C",
+        "#B3E1A3",
+        "#EADDA0",
+        "#A4B9EF",
+        "#C6AAE8",
+        "#F0AFE1",
+        "#D7DAE0",
+      },
+
+      tab_bar = {
+        background = "#15121C",
+        active_tab = {
+          bg_color = "#1E1E28",
+          fg_color = "#D7DAE0",
+        },
+        inactive_tab = {
+          bg_color = "#1B1923",
+          fg_color = "#A4B9EF",
+        },
+        inactive_tab_hover = {
+          bg_color = "#1E1E28",
+          fg_color = "#D7DAE0",
+        },
+        new_tab = {
+          bg_color = "#15121C",
+          fg_color = "#6E6C7C",
+        },
+        new_tab_hover = {
+          bg_color = "#1E1E28",
+          fg_color = "#D7DAE0",
+          italic = true,
+        },
+      },
+    },
+  },
   font = wezterm.font_with_fallback {
     "Fira Code",
     "Symbols Nerd Font",
