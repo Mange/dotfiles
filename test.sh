f65dd151eae187a3ef75581316f216b120a33647
Magnus Bergmark <me@mange.dev>
2021-12-31 13:59:21 +0100

    Ansible: Improve test runner by reusing containers
    Now I can keep reusing the same container over and over until I ask it
    to rebuild it. This saves on time and bandwidth as I keep adding more
    and more things to my Ansible setup.

--- test.sh
+++ test.sh
@@ -1,29 +1,64 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "--help" ] || [ "$#" -eq 0 ]; then
-  echo "Usage: $0 <platform>"
-  echo ""
-  echo "Test the setup of <platform> using Docker."
-  echo ""
-  echo "Known platforms:"
-  echo "- arch"
-  [ "$1" = "--help" ] && exit 0
-  exit 1
-fi
+usage() {
+  cat <<EOF
+Usage: $0 [options] <platform>
 
-platform="$1"
-case "$platform" in
-arch)
-  docker build -t dotfiles-arch -f test/Dockerfile.arch .
+Test the setup of <platform> using Docker.
+
+Known platforms:
+  - arch
+
+Options:
+  --rebuild
+    Rebuild the container before starting it, even if it already exist.
+EOF
+}
+
+mode=reuse
+platform=
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+  --help)
+    shift
+    usage
+    exit 0
+    ;;
+  --rebuild)
+    mode=rebuild
+    shift
+    ;;
+  arch)
+    platform=arch
+    shift
+    ;;
+  *)
+    echo "Unknown option: $1" >&2
+    usage >&2
+    exit 1
+    ;;
+  esac
+done
+
+image_name="dotfiles-$platform"
+container_name="dotfiles-$platform-run"
+
+has_container() {
+  count="$(docker ps --quiet --all --filter name="$1" | wc -l)"
+  [ "$count" -gt 0 ]
+}
+
+if has_container "$container_name" && [ "$mode" = "reuse" ]; then
+  exec docker start --interactive --attach "$container_name"
+else
+  docker build -t "$image_name" -f "test/Dockerfile.${platform}" .
+  (has_container "$container_name" && docker rm "$container_name") || true
+
   exec docker run \
-    --rm -ti \
+    -ti \
+    --name "$container_name" \
     --volume "$(pwd)/ansible:/home/mange/Projects/dotfiles/ansible" \
-    dotfiles-arch \
-    zsh
-  ;;
-*)
-  echo "Unknown platform $platform"
-  exit 1
-  ;;
-esac
+    "$image_name"
+fi
