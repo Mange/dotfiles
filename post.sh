4446a717e7aa2aeb0058dbc1984c046e5610d479
Magnus Bergmark <me@mange.dev>
2022-01-07 20:30:08 +0100

    Ansible: Handle the last of post.sh

--- post.sh
+++ post.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 # Run after installing dotfiles to perform extra work.
 
-# Clean up left-overs, if found
-[ -d ~/.fzf ] && rm -rf ~/.fzf
-rm -f ~/.fzf.*
-
-# Migrate some XDG stuff
-migrate_xdg() {
-  local from="$1"
-  local to="$2"
-
-  if [[ -n "$to" ]] && [[ -d "$from" ]] && ! [[ -d "$to" ]]; then
-    echo "Moving $from → $to"
-    mkdir -p "$(dirname "$to")" && mv "$from" "$to"
-  fi
-}
-migrate_xdg ~/go "$GOPATH"
-migrate_xdg ~/.cargo "$CARGO_HOME"
-migrate_xdg ~/.rustup "$RUSTUP_HOME"
-migrate_xdg ~/.gnupg "$GNUPGHOME"
-
 if [[ "$SHELL" != *zsh ]]; then
   echo "Warning: You seem to be using a shell different from zsh (${SHELL})" >/dev/stderr
   echo "Fix this by running:" >/dev/stderr
