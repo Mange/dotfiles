7c83c9f12e3d3034334d327db203d7ba27b8ada2
Magnus Bergmark <me@mange.dev>
2022-01-07 20:31:31 +0100

    Remove dotfiles-cli

--- post.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Run after installing dotfiles to perform extra work.
-
-if [[ "$SHELL" != *zsh ]]; then
-  echo "Warning: You seem to be using a shell different from zsh (${SHELL})" >/dev/stderr
-  echo "Fix this by running:" >/dev/stderr
-  echo "  chsh -s \$(which zsh)" >/dev/stderr
-fi
