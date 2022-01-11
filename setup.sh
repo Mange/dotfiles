799a259505b92ad3613e8c413780951cdc9d9558
Magnus Bergmark <me@mange.dev>
2022-01-11 12:56:29 +0100

    Add dotfiles cli

--- setup.sh
+++ setup.sh
@@ -8,40 +8,8 @@ if [ "$1" = "--help" ]; then
   exit 0
 fi
 
-setup_arch() {
-  if ! hash ansible-playbook >/dev/null 2>/dev/null; then
-    sudo pacman -S --noconfirm --needed ansible
-  fi
-}
-
-case "$(uname -s)" in
-Linux)
-  if [ -f /etc/os-release ]; then
-    dist=$(awk -F '=' '/^ID=/ { print $2 }' /etc/os-release)
-    case "$dist" in
-    arch*)
-      setup_arch
-      ;;
-    *)
-      echo "Not a supported dist: $dist"
-      exit 1
-      ;;
-    esac
-  fi
-  ;;
-*)
-  echo "Not a supported system."
-  exit 1
-  ;;
-esac
-
 dir="$(dirname "$(readlink -f "$0")")"
 cd "$dir"
+eval "$(bin/dotfiles --env)"
 
-ansible-galaxy collection install -r requirements.yml
-
-ansible-playbook \
-  --connection=local --inventory localhost, \
-  --ask-become-pass \
-  environment.yml \
-  "$@"
+exec bin/dotfiles-setup
