c745642ffb96c3cd2ccc2f6537c71784b76b32aa
Magnus Bergmark <me@mange.dev>
2022-01-25 13:59:06 +0100

    Wezterm: Improve keybinds

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -32,4 +32,90 @@ return {
   },
   hide_tab_bar_if_only_one_tab = true,
   window_background_opacity = 0.8,
+
+  disable_default_key_bindings = true,
+  keys = {
+    { mods = "CTRL|SHIFT", key = "z", action = "QuickSelect" },
+    {
+      mods = "CTRL|SHIFT",
+      key = "x",
+      action = "ActivateCopyMode",
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "e",
+      action = wezterm.action {
+        QuickSelectArgs = {
+          label = "open url",
+          patterns = {
+            "https?://\\S+",
+          },
+          action = wezterm.action_callback(function(window, pane)
+            local url = window:get_selection_text_for_pane(pane)
+            wezterm.open_with(url)
+          end),
+        },
+      },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "c",
+      action = wezterm.action { CopyTo = "ClipboardAndPrimarySelection" },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "v",
+      action = wezterm.action { PasteFrom = "Clipboard" },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "s",
+      action = wezterm.action { PasteFrom = "PrimarySelection" },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "f",
+      action = wezterm.action { Search = { CaseInSensitiveString = "" } },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "r",
+      action = wezterm.action { Search = { Regex = "" } },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "-",
+      action = "DecreaseFontSize",
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "=",
+      action = "IncreaseFontSize",
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "0",
+      action = "ResetFontSize",
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "PageUp",
+      action = wezterm.action { ScrollByPage = -1 },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "PageDown",
+      action = wezterm.action { ScrollByPage = 1 },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "UpArrow",
+      action = wezterm.action { ScrollToPrompt = -1 },
+    },
+    {
+      mods = "CTRL|SHIFT",
+      key = "DownArrow",
+      action = wezterm.action { ScrollToPrompt = 1 },
+    },
+  },
 }
