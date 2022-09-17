aee70e7b9cc4cc18137f56c68e88d4f26687657d
Magnus Bergmark <me@mange.dev>
2022-09-18 00:58:30 +0200

    Awesome: media mode: Show music information

--- config/awesome/media_mode.lua
+++ config/awesome/media_mode.lua
@@ -1,7 +1,25 @@
 local which_keys = require "module.which_keys" --[[@as WhichKeys]]
 local actions = require "actions"
+local wibox = require "wibox"
+local ui_content = require "widgets.media-info.content"
+local utils = require "utils"
+local dpi = utils.dpi
 
-local media_mode = which_keys.new_chord("media", {
+local media_mode = which_keys.new_chord("Media", {
+  widget_top = wibox.widget {
+    widget = wibox.container.margin,
+    left = dpi(20),
+    top = dpi(20),
+    bottom = 0,
+    right = dpi(20),
+    {
+      layout = wibox.layout.fixed.horizontal,
+      spacing = dpi(10),
+      forced_height = dpi(96),
+      ui_content.album_cover.widget,
+      ui_content.song_info.music_info,
+    },
+  },
   keybindings = {
     -- Playerctl
     which_keys.key(
