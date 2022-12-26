1318141b83ba3a8cc19dce990e46b14183aa9772
Magnus Bergmark <me@mange.dev>
2022-12-26 20:59:48 +0100

    Awesome: Split which_key module in multiple files
    Division is still not very clear, but I hope to refactor this better
    soon when I have more of the features implemented.

---
+++ config/awesome/module/which_keys/items.lua
@@ -0,0 +1,64 @@
+local keylib = require "module.keys.lib"
+
+--- @class WhichKeyItem
+--- @field bind KeyBind
+--- @field name string
+--- @field type WhichKeyType
+--- @field action function()
+
+local M = {}
+
+--- @param bind KeyBind
+--- @param opts WhichKeyOpts
+local function menu_item(bind, opts, menu)
+  local child_menu = menu:create_child_menu(opts)
+
+  return {
+    bind = bind,
+    name = menu.name,
+    type = "menu",
+    menu = child_menu,
+    action = function()
+      menu:stop()
+      child_menu:start()
+    end,
+  }
+end
+
+local function action_item(bind, opts, menu)
+  local action = opts[1]
+  local name = opts[2]
+
+  return {
+    bind = bind,
+    name = name,
+    type = "action",
+    action = function()
+      menu:stop()
+      action()
+    end,
+  }
+end
+
+function M.parse(key, item, menu)
+  local bind = keylib.parse_bind(key)
+
+  if item["name"] then
+    -- item is a nested menu
+    return menu_item(bind, item, menu)
+  else
+    return action_item(bind, item, menu)
+  end
+end
+
+function M.parse_list(keys, menu)
+  local items = {}
+
+  for key, bind in pairs(keys) do
+    table.insert(items, M.parse(key, bind, menu))
+  end
+
+  return items
+end
+
+return M
