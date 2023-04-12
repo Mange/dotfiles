7a27590ab10f1e4b511b24811bf4087b019488ec
Magnus Bergmark <me@mange.dev>
2023-04-12 14:06:11 +0200

    Neovim: Fix statuscol

--- config/nvim/lua/plugins/folding.lua
+++ config/nvim/lua/plugins/folding.lua
@@ -60,10 +60,23 @@ return {
   {
     "luukvbaal/statuscol.nvim",
     lazy = false,
-    opts = {
-      foldfunc = "builtin",
-      -- order = "SNsFs", -- Symbol, Number, Fold, separator
-      setopt = true,
-    },
+    config = function()
+      local builtin = require "statuscol.builtin"
+      require("statuscol").setup {
+        segments = {
+          -- Symbol
+          { text = { "%s" }, click = "v:lua.ScSa" },
+          -- Fold
+          -- { text = { "%C" }, click = "v:lua.ScFa" },
+          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+          -- Line number
+          {
+            text = { builtin.lnumfunc, " " },
+            condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+          },
+        },
+      }
+    end,
   },
 }
