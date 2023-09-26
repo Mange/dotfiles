41b6fca50d16cd60055a53f5736dd4f84c55dd69
Magnus Bergmark <me@mange.dev>
2023-09-26 08:32:03 +0200

    Neovim: Disable UFO+folding in some buffers

--- config/nvim/lua/plugins/folding.lua
+++ config/nvim/lua/plugins/folding.lua
@@ -48,6 +48,15 @@ local function config()
     },
     fold_virt_text_handler = virtual_text_handler,
   }
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "NeogitPopup", "Outline", "Trouble" },
+    callback = function()
+      require("ufo").detach()
+      vim.opt_local.foldenable = false
+      vim.wo.foldcolumn = "0"
+    end,
+  })
 end
 
 return {
