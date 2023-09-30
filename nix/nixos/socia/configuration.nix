95d8b8a6251cd55323ba0d3c331dcf1e4b35fa4d
Magnus Bergmark <me@mange.dev>
2023-09-30 21:31:03 +0200

    Nix: Auto GC

--- nix/nixos/socia/configuration.nix
+++ nix/nixos/socia/configuration.nix
@@ -55,6 +55,12 @@
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
   };
 
   networking.hostName = "socia";
