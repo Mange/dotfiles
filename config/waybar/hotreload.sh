740cc195178edc1869472c6d0a828e8898ff86a5
Magnus Bergmark <me@mange.dev>
2023-09-30 21:31:10 +0200

    Waybar: Fix and improve hotreload script

--- config/waybar/hotreload.sh
+++ config/waybar/hotreload.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-
-CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
-killall waybar
-# trap "killall waybar" EXIT
-
-while true; do
-  waybar "$@" &
-  inotifywait -e create,modify $CONFIG_FILES
-  killall waybar
-done
+#!/usr/bin/env bash
+exec watchexec --clear -- 'pkill waybar || true; waybar &'
