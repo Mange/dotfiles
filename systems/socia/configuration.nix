43fc71b295aae9dcd53f1c2862da283c3666eacd
Magnus Bergmark <me@mange.dev>
2023-10-04 15:48:21 +0200

    Socia: Upgrade to Linux 6.5

--- systems/socia/configuration.nix
+++ systems/socia/configuration.nix
@@ -38,6 +38,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # AMD RX 7900 XT
-  boot.kernelPackages = pkgs.linuxPackages_6_4; # Works better with newer kernels…
+  boot.kernelPackages = pkgs.linuxPackages_6_5; # Works better with newer kernels…
   hardware.amdgpu.opencl = true;
 }
