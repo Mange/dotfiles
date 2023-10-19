ab2e3f01241671b6385d167f613996abc7f7d646
Magnus Bergmark <me@mange.dev>
2023-10-19 02:04:43 +0200

    Nix: Move over git and locale to home-manager

--- home/_roles/pc/default.nix
+++ home/_roles/pc/default.nix
@@ -10,6 +10,7 @@ in {
     # New
     ./catppuccin.nix
     ./cli.nix
+    ./git.nix
     ./security.nix
 
     ../../_topics/fonts
@@ -18,94 +19,92 @@ in {
     ../../_topics/toys.nix
   ];
 
-  home = {
-    language.base = "sv_SE.UTF-8";
-
-    # Setup symlinks.
-    file.".face".source = ./face.jpg;
-
-    packages = with pkgs; [
-      # Building software common dependencies
-      shared-mime-info
-
-      # Bluetooth
-      bluez
-      bluez-tools
-      blueman
-
-      # Network
-      curl
-      httpie
-      nmap
-      rsync
-
-      # Other hardware
-      lm_sensors
-
-      # Modern UNIX replacements
-      bat
-      fd
-      htop
-      prettyping
-      procs
-      ripgrep
-
-      # CLI
-      duplicity
-      file
-      glib # for "gio trash", etc.
-      gnuplot
-      killall
-      libqalculate
-      lsof
-      psmisc # better pstree command
-      wget
-
-      # Dev
-      delta
-      git
-      git-absorb
-      github-cli
-      gnome.zenity
-      http-prompt
-      hub
-      jq
-      nodejs
-      parallel
-      pastel
-      pgcli
-      rustup
-      watchexec
-
-      cargo-update
-      cargo-edit
-      cargo-watch
-
-      # Archives
-      atool
-      p7zip
-      unrar
-      unzip
-      xz
-      zip
-
-      # Gaming
-      steam
-
-      # Mobile integration
-      scrcpy
-      android-file-transfer
-
-      # Media
-      mediainfo
-      youtube-dl
-      imagemagick # for image previews, etc.
-      ghostscript # imagemagick optional dependency for PDF support
-
-      # Other
-      monero
-    ];
+  home.language = {
+    base = "sv_SE.UTF-8";
+    time = "sv_SE.UTF-8";
+    collate = "sv_SE.UTF-8";
   };
+
+  # Setup symlinks.
+  home.file.".face".source = ./face.jpg;
+  xdg.dataFile."wallpapers" = {
+    source = ./wallpapers;
+    recursive = true;
+  };
+
+  home.packages = with pkgs; [
+    # Bluetooth
+    bluez
+    bluez-tools
+    blueman
+
+    # Network
+    curl
+    httpie
+    nmap
+    rsync
+
+    # Other hardware
+    lm_sensors
+
+    # Modern UNIX replacements
+    bat
+    fd
+    htop
+    prettyping
+    procs
+    ripgrep
+
+    # CLI
+    duplicity
+    file
+    glib # for "gio trash", etc.
+    gnuplot
+    killall
+    libqalculate
+    lsof
+    psmisc # better pstree command
+    wget
+
+    # Dev
+    gnome.zenity
+    http-prompt
+    jq
+    nodejs
+    parallel
+    pastel
+    pgcli
+    rustup
+    watchexec
+
+    cargo-update
+    cargo-edit
+    cargo-watch
+
+    # Archives
+    atool
+    p7zip
+    unrar
+    unzip
+    xz
+    zip
+
+    # Gaming
+    steam
+
+    # Mobile integration
+    scrcpy
+    android-file-transfer
+
+    # Media
+    mediainfo
+    youtube-dl
+    imagemagick # for image previews, etc.
+    ghostscript # imagemagick optional dependency for PDF support
+
+    # Other
+    monero
+  ];
 
   # config/ directory
   # xdg.configFile = lib.pipe (utils.filesIn ../config) [
@@ -115,7 +114,6 @@ in {
   #   (utils.mergeAttrs)
   # ];
   xdg.configFile = {
-    "git".source = utils.linkConfig "git";
     "mako".source = utils.linkConfig "mako";
     "pgcli".source = utils.linkConfig "pgcli";
     "procs".source = utils.linkConfig "procs";
@@ -125,12 +123,6 @@ in {
     "waybar".source = utils.linkConfig "waybar";
     "xkb".source = utils.linkConfig "xkb";
 
-    "locale.conf".source = ../../../config/locale.conf;
     "user-dirs.dirs".source = ../../../config/user-dirs.dirs.template;
-  };
-
-  xdg.dataFile."wallpapers" = {
-    source = ./wallpapers;
-    recursive = true;
   };
 }
---
+++ home/_roles/pc/git.nix
@@ -0,0 +1,91 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    delta
+    git-absorb
+    github-cli
+    hub # mostly for `git sync`; I should migrate away
+  ];
+
+  programs.git = {
+    enable = true;
+
+    userName = "Magnus Bergmark";
+    userEmail = "me@mange.dev";
+
+    aliases = {
+      prune = "!git remote | xargs -n 1 git remote prune";
+    };
+
+    signing = {
+      signByDefault = true;
+      key = "2EA6F4AA110A1BF2227519A90443C69F6F022CDE";
+    };
+
+    extraConfig = {
+      github.user = "Mange";
+
+      # Newspeak is double-plus ungood
+      init.defaultBranch = "master";
+
+      mergetool = {
+        prompt = true;
+        keepBackup = false;
+        vimdiff = {
+          cmd = ''nvim -d "$LOCAL" "$BASE" "$REMOTE" "$MERGED" -c "\$wincmd w" -c "wincmd J" -c "windo set wrap"'';
+        };
+      };
+
+      merge = {
+        conflictstyle = "diff3";
+        tool = "vimdiff";
+      };
+
+      diff.tool = "vimdiff";
+
+      rebase = {
+        autosquash = true;
+        stat = true;
+      };
+
+      log = {
+        showSignature = false;
+        date = "format:%Y-%m-%d %H:%M";
+      };
+
+      color.branch = {
+        remote = "blue";
+        current = "bold green";
+        local = "green";
+      };
+    };
+
+    ignores = [
+      #-- Vim doc tags --
+      "doc/tags"
+
+      #-- Crap and metadata --
+      ".DS_Store"
+      "*~"
+
+      #-- Project-specific things --
+      ".*.local"
+      "notes.local"
+      ".ruby-gemset"
+
+      #-- Editor-specific things --
+      "*.swp"
+      ".vimrc"
+      ".nvimrc"
+      ".*.rs.rustfmt"
+      ".#*"
+
+      #-- direnv --
+      ".envrc"
+      ".direnv"
+    ];
+
+    delta = {
+      enable = true;
+    };
+  };
+}
