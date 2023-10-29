9593e91dc69d06716fd1c7a8e4901a18b38fa1be
Magnus Bergmark <me@mange.dev>
2023-10-29 20:23:00 +0100

    Remove my own wmquit and use wlogout instead

--- home/_roles/pc/wayland.nix
+++ home/_roles/pc/wayland.nix
@@ -20,6 +20,50 @@ in {
     longitude = 17.8;
   };
 
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        label = "lock";
+        action = "wl-lock-screen";
+        text = "Lock";
+        keybind = "1";
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "Suspend";
+        keybind = "2";
+      }
+      {
+        label = "logout";
+        action = "loginctl terminate-user $USER";
+        text = "Logout";
+        keybind = "3";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "4";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "5";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "6";
+      }
+    ];
+    style = ./wlogout/style.css;
+  };
+  xdg.configFile."wlogout/icons".source = ./wlogout/icons;
+
   # Utilities for wayland
   home.packages = with pkgs; [
     # Screenshotting, screen recording, etc.
