2a3e06da7d9c883eaf2c7efba3da9f10415e2f3b
Magnus Bergmark <me@mange.dev>
2023-10-31 12:34:16 +0100

    Neovim: Configure defaults in Markdown files

--- config/nvim/lua/mange/autocommands/init.lua
+++ config/nvim/lua/mange/autocommands/init.lua
@@ -9,8 +9,10 @@ vim.api.nvim_create_autocmd("VimResized", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "markdown" },
   callback = function()
+    vim.opt_local.shiftwidth = 2
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+    -- Line numbers does not look good when editing text.
     vim.opt_local.number = false
   end,
 })
