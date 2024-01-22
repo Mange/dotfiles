8106172dc858f2e5ab7a39d81b3093f2c0576c89
Magnus Bergmark <me@mange.dev>
2024-01-22 20:50:12 +0100

    Nix: Laptops should get brightnessctl and powertop

--- systems/_roles/laptop/default.nix
+++ systems/_roles/laptop/default.nix
@@ -1,7 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ../pc
 
     ../../_topics/bluetooth.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    powertop
+    brightnessctl
   ];
 }
