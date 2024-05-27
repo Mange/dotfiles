5906184164f11cc382699c4c52e1dc0469bdde22
Magnus Bergmark <me@mange.dev>
2024-05-27 21:20:19 +0200

    Nix: Fix `npm install -g`
    This is too useful to skip when working on projects. Would be cool to
    install everything through Nix, but not when I'm developing custom
    software for work.

--- systems/_roles/pc/programs.nix
+++ systems/_roles/pc/programs.nix
@@ -43,4 +43,12 @@
   # Docker
   virtualisation.docker.enable = true;
   users.users.mange.extraGroups = ["docker"];
+
+  # NPM should be integrated with Nixos so global installs use a custom prefix.
+  programs.npm = {
+    enable = true;
+    npmrc = ''
+      prefix = ''${HOME}/.local/share/npm
+    '';
+  };
 }
