702bc96dd5308d77c26f1448b5a7626942d1ef1f
Magnus Bergmark <me@mange.dev>
2024-07-06 21:07:21 +0200

    Neovim: Add markdownview plugin

---
+++ config/nvim/lua/plugins/ui/markdownview.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      checkboxes = { enable = false },
+    },
+  },
+}
