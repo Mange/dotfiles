40b78b38c7f029e029330714c695d2f0f8d6f561
Magnus Bergmark <me@mange.dev>
2025-02-22 21:46:53 +0100

    Hyprland: Fix Bitwarden window script
    Now it should work on both Swedish and English locale versions of the
    browser, and it also uses a more straightforward implementation. It
    should also not double-apply (once for `windowtitle` and once for
    `windowtitlev2`).

--- home/_roles/pc/hyprland.nix
+++ home/_roles/pc/hyprland.nix
@@ -7,37 +7,34 @@
   hyprland = pkgs.hyprland;
   hyprplugins = pkgs.hyprlandPlugins;
 
+  # Based on:
   # https://github.com/hyprwm/Hyprland/issues/3835
+  # Improved later by me.
   bitwardenResizeScript = pkgs.writeScript "hyprland-bitwarden-resize" /* sh */ ''
-    #!/bin/sh
+    #!${pkgs.bash}/bin/bash
     handle() {
       case $1 in
-        windowtitle*)
-          # Extract the window ID from the line
-          window_id=''${1#*>>}
-
-          # Fetch the list of windows and parse it using jq to find the window by its decimal ID
-          window_info=$(hyprctl clients -j | ${pkgs.jq}/bin/jq --arg id "0x$window_id" '.[] | select(.address == ($id))')
-
-          # Extract the title from the window info
-          window_title=$(echo "$window_info" | ${pkgs.jq}/bin/jq '.title')
+        # Example line:
+        # windowtitlev2>>35e360a0,Tillägg: (Bitwarden Lösenordshanterare) - — Mozilla Firefox
+        #    event     ││ win ID │win title
+        "windowtitlev2>>"*)
+          # Extract the window ID and title from the line
+          payload="''${1#windowtitlev2>>}"
+          comma_pos=$(expr index "$payload" ,)
+          window_id="''${payload:0:comma_pos-1}"
+          window_title="''${payload:comma_pos}"
 
           # Check if the title matches the characteristics of the Bitwarden popup window
-          if [[ "$window_title" == *"(Bitwarden - Free Password Manager) - Bitwarden"* ]]; then
-          
-            # echo $window_id, $window_title
-            # hyprctl dispatch togglefloating address:0x$window_id
-            # hyprctl dispatch resizewindowpixel exact 20% 40%,address:0x$window_id
-            # hyprctl dispatch movewindowpixel exact 40% 30%,address:0x$window_id
-
-            hyprctl --batch "dispatch togglefloating address:0x$window_id ; dispatch resizewindowpixel exact 20% 40%,address:0x$window_id"
-          fi
-          ;;
+          case "$window_title" in
+            *": (Bitwarden - Free Password Manager)"* | *": (Bitwarden Lösenordshanterare)"* )
+              hyprctl --batch "dispatch setfloating address:0x$window_id ; dispatch resizewindowpixel exact 20% 40%,address:0x$window_id"
+              ;;
+          esac
       esac
     }
 
     # Listen to the Hyprland socket for events and process each line with the handle function
-    ${pkgs.socat}/bin/socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
+    ${pkgs.socat}/bin/socat -U - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
   '';
 in 
 {
