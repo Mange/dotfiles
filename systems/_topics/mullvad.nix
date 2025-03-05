6d04d0745e4d3742fe913177828c8febddd0f1ce
Magnus Bergmark <me@mange.dev>
2025-03-05 16:55:32 +0100

    NixOS: Enable Mullvad again; bug has been fixed

--- systems/_topics/mullvad.nix
+++ systems/_topics/mullvad.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }: {
-  # Broken right now https://github.com/NixOS/nixpkgs/issues/385996
-  # services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.enable = true;
 
   environment.systemPackages = with pkgs; [
-    # mullvad
-    # mullvad-vpn
+    mullvad
+    mullvad-vpn
     mullvad-browser
   ];
 }
