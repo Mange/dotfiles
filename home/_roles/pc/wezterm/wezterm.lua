6af9d9649b94d72e448302b85615fd47f578fc78
Magnus Bergmark <me@mange.dev>
2025-04-16 09:40:44 +0200

    Wezterm: Default to smaller font size

--- home/_roles/pc/wezterm/wezterm.lua
+++ home/_roles/pc/wezterm/wezterm.lua
@@ -24,6 +24,7 @@ return {
     THEME = env_scheme,
   },
   font = font "Jetbrains Mono",
+  font_size = 10.0,
   hide_tab_bar_if_only_one_tab = true,
   window_background_opacity = 0.9, -- Until Niri gets blurred backgrounds
 
