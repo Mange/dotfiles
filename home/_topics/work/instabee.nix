d556d72429157a6b0a90d4059b1053a1b35eb145
Magnus Bergmark <me@mange.dev>
2025-07-10 08:19:36 +0200

    Add mycli to Instabee work machines

--- home/_topics/work/instabee.nix
+++ home/_topics/work/instabee.nix
@@ -23,5 +23,37 @@
     mongodb-compass
     postgresql
     mariadb
+    mycli
   ];
+
+  home.file.".myclirc".text =  /*ini*/ ''
+    [main]
+    # Only execute query on Return when it ends with a semicolon
+    multi_line = True
+
+    # fo' life!
+    key_bindings = vi
+
+    # Warn on things like "DROP TABLE".
+    destructive_warning = True
+
+    # Like \x auto in psql; transpose table when few rows.
+    auto_expand = True
+
+    # I'm weird LIKE that
+    keyword_casing = upper
+
+    less_chatty = True
+    table_format = fancy_grid
+    syntax_style = manni
+
+    enable_pager = True
+    wider_completion_menu = True
+    auto_vertical_output = True
+
+    # More XDG-like; generated files should not go in ~/.config
+    log_file = ~/.local/share/mycli/log
+    history_file = ~/.local/share/mycli/history
+    casing_file = ~/.local/share/mycli/casing
+  '';
 }
