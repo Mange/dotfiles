6bab46244b40f0e918c75859a2fa4472dbf1d2fd
Magnus Bergmark <me@mange.dev>
2025-08-28 11:00:55 +0200

    Neovim: Configure Markview some more

--- config/nvim/lua/plugins/ui/markdownview.lua
+++ config/nvim/lua/plugins/ui/markdownview.lua
@@ -12,6 +12,16 @@ return {
 
     opts = {
       checkboxes = { enable = false },
+      preview = {
+        icon_provider = "devicons",
+        hybrid_modes = { "i" },
+      },
+      markdown = {
+        list_items = {
+          marker_minus = { add_padding = false },
+          marker_dot = { add_padding = false },
+        },
+      },
     },
   },
 }
