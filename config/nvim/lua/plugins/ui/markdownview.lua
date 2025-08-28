8de3ad90eccb6148eb14be17a88be744494a0bee
Magnus Bergmark <me@mange.dev>
2025-08-28 11:00:47 +0200

    Neovim: Bump plugins

--- config/nvim/lua/plugins/ui/markdownview.lua
+++ config/nvim/lua/plugins/ui/markdownview.lua
@@ -1,10 +1,15 @@
 return {
   {
     "OXY2DEV/markview.nvim",
+    lazy = false,
+
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
     },
+
+    -- So nvim-treesitter is loaded first
+    priority = 49,
+
     opts = {
       checkboxes = { enable = false },
     },
