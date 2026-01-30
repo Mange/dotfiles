6cfb3b1c6e1e173ff4318554e2e85c0f213f90c2
Magnus Bergmark <me@mange.dev>
2026-01-30 08:21:41 +0100

    Neovim: Add support for SurrealQL / SurrealDB

---
+++ config/nvim/lua/plugins/langs/surrealdb.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "dariuscorvus/tree-sitter-surrealdb.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      -- setup step
+      require("tree-sitter-surrealdb").setup()
+    end,
+  },
+}
