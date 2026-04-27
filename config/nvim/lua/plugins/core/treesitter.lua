ec10a9f5ab75c05e20761d27df1f909a8fc3546f
Magnus Bergmark <me@mange.dev>
2026-04-27 22:31:58 +0200

    Neovim: Use Treesitter on supported fts

--- config/nvim/lua/plugins/core/treesitter.lua
+++ config/nvim/lua/plugins/core/treesitter.lua
@@ -4,5 +4,16 @@ return {
     branch = "main",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local lang =
+            vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+          if lang then
+            pcall(vim.treesitter.start, args.buf, lang)
+          end
+        end,
+      })
+    end,
   },
 }
